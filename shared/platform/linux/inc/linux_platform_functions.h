d859f04214753be1fdeb9e05976fb69b43e3ac4e
acichy2281 <ajcichy@gmail.com>
2025-02-17 11:27:55 -0500

    Adds linux platform functions

---
+++ shared/platform/linux/inc/linux_platform_functions.h
@@ -0,0 +1,12 @@
+#ifndef _LINUX_PLATFORM_FUNCTIONS_H_
+#define _LINUX_PLATFORM_FUNCTIONS_H_
+
+/* System include */
+#include <iostream>
+
+/* Platform includes */
+#include <termios.h>
+
+bool Linux_IsUserQuit();
+
+#endif // _LINUX_PLATFORM_FUNCTIONS_H_
